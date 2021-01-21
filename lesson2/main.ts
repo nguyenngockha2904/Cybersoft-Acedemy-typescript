4127eda98bc77dcf3afc88ea018ffdeab3c0738c
Kha Nguyen <khanguyen1000@gmail.com>
2021-01-22 00:38:31 +0700

    update lesson 2

--- lesson2/main.ts
+++ lesson2/main.ts
@@ -1,18 +1,79 @@
+let username = 'Kha';
+let age = 22;
+let trangThai = true;
+
+// username=12;
+username = '';
 // string
-let character: string = 'cybersoft';
-let strg: string;
-// character=12;
-// character=true;
-character = '';
-// str=12;
+let str: string;
+str = username;
 
 // number
-let st = 12;
-let age: number;
+let num: number;
+num = age;
+
 // boolean
 let bool: boolean;
+bool = trangThai;
 
-const chuvi = (duongkinh: number) => {
-  return duongkinh * Math.PI;
+// Array
+let strings = ['A', 'B', 'C', 'D'];
+
+// strings.push(1);
+strings.push('E');
+
+// cú pháp 1
+let arrStr: Array<string> = strings;
+// cú pháp 2
+let arrStr2: string[] = strings;
+
+let numbers = [1, 2, 3, 4];
+// numbers.push('A');
+// numbers[1]='';
+numbers.push(6);
+
+// cú pháp 1
+let arrNums: Array<number> = numbers;
+// cú pháp 2
+let arrNums2: number[] = numbers;
+
+// mang mixed union type
+let mixed = [1, 'A', 2, 'D'];
+mixed.push(1);
+mixed.push('E');
+// mixed.push(true);
+mixed[0] = 'A';
+mixed[1] = 5;
+
+// cú pháp 1
+let arrayMixed: Array<number | string> = mixed;
+let arrayMixed2: (number | string)[] = mixed;
+
+// object
+
+let product = {
+  id: 1,
+  name: 'Máy giặt',
+  price: 1000,
+  isSold: false,
 };
-console.log(chuvi(12));
+
+product.name = 'tủ lạnh';
+product.price = 2500;
+// product.isSold='asd';
+// product.weight=159;
+
+product = {
+  id: 1,
+  name: 'tivi',
+  price: 12,
+  isSold: false,
+};
+let newProduct: {
+  id: number;
+  name: string;
+  price: number;
+  isSold: boolean;
+};
+
+newProduct = product;
