9a23031559769f82649bd71f9259101f41e7e3a8
Kha Nguyen <khanguyen1000@gmail.com>
2021-01-21 15:39:04 +0700

    upload layout ql phieu nhap xuat

---
+++ main.js
@@ -0,0 +1,8 @@
+var character = 'Kha Cybersoft-Cyberlearn';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
+console.log('kha dep trai');
